3857886677aeae97a80e757855435c43d1d3400f
Jason Barry <jasondanielbarry512@gmail.com>
2024-06-08 15:15:42 +0200

    (1)

--- VCLComponents/Drawing/Skia/SkiaDrawingMethods.pas
+++ VCLComponents/Drawing/Skia/SkiaDrawingMethods.pas
@@ -6,8 +6,20 @@ interface
         //Delphi
             System.Skia, Vcl.Skia,
         //custom
+            DrawingAxisConversionClass,
             GeometryTypes, GeomPolyLineClass;
 
 implementation
+
+    //draw polyline
+        procedure drawPolyLine( const polylineIn        : TGeomPolyLine;
+                                const axisConverterIn   : TDrawingAxisConverter;
+                                const paintIn           : ISkPaint;
+                                var canvasInOut         : ISkCanvas             );
+            var
+                pathbuilder : ISkPathBuilder;
+            begin
+
+            end;
 
 end.
