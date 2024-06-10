49543737b4bc87a651c57677b5fd6a05ead3cdb1
Jason Barry <jasonbarry@prokon.com>
2024-06-10 11:18:52 +0200

    (1) working on SkiaDrawingMethods.pas

--- VCLComponents/Drawing/Skia/SkiaDrawingMethods.pas
+++ VCLComponents/Drawing/Skia/SkiaDrawingMethods.pas
@@ -4,6 +4,7 @@ interface
 
     uses
         //Delphi
+            system.types, system.UITypes,
             System.Skia, Vcl.Skia,
         //custom
             DrawingAxisConversionClass,
@@ -12,14 +13,47 @@ interface
 implementation
 
     //draw polyline
-        procedure drawPolyLine( const polylineIn        : TGeomPolyLine;
-                                const axisConverterIn   : TDrawingAxisConverter;
-                                const paintIn           : ISkPaint;
-                                var canvasInOut         : ISkCanvas             );
+        procedure drawSkiaPolygon(  const polylineIn        : TGeomPolyLine;
+                                    const colourIn          : TAlphaColor;
+                                    const axisConverterIn   : TDrawingAxisConverter;
+                                    var canvasInOut         : ISkCanvas             );
             var
-                pathbuilder : ISkPathBuilder;
+                drawingPoints   : TArray<TPointF>;
+                pathbuilder     : ISkPathBuilder;
+                path            : ISkPath;
+                paint           : ISkPaint;
             begin
 
+                //convert geometry into canvas drawing points
+                    drawingPoints := axisConverterIn.arrXY_to_arrLTF(
+                                                                        polylineIn.drawingPoints()
+                                                                    );
+
+                //define the drawing path
+                    pathbuilder := TSkPathBuilder.Create();
+
+                    pathbuilder.MoveTo(drawingPoints[0]);
+
+                    pathbuilder.PolylineTo(drawingPoints);
+
+                    pathbuilder.LineTo(drawingPoints[0]);
+                    pathbuilder.LineTo(drawingPoints[1]);
+
+                    path := pathbuilder.Detach();
+
+                //draw the shape
+                    paint := TSkPaint.create();
+
+                    //line
+                        paint.Style         := TSkPaintStyle.Stroke;
+                        paint.Color         := TAlphaColors.Black;
+                        paint.StrokeWidth   := 2;
+                        canvasInOut.DrawPath(path, paint);
+
+                    //fill
+                        paint.Style         := TSkPaintStyle.Fill;
+                        paint.Color         := colourIn;
+                        canvasInOut.DrawPath(path, paint);
             end;
 
 end.
