a53909d4807ce76081d5b412fa103b65a1c31b78
Jason Barry <jasondanielbarry512@gmail.com>
2024-09-01 20:11:17 +0200

    (1) Gauss elimination working

--- GeneralMath/LinearAlgebra/Matrices/MatrixDeterminantMethods.pas
+++ GeneralMath/LinearAlgebra/Matrices/MatrixDeterminantMethods.pas
@@ -22,6 +22,146 @@ interface
 implementation
 
     //determinant
+        //Gauss Elimination
+            //subtract factored row
+                procedure factoredRowSubtraction(   const rowAboveIn, rowBelowIn    : integer;
+                                                    var matrixInOut                 : TLAMatrix );
+                    var
+                        c, rA, rB           : integer;
+                        factor              : double;
+                        size                : TLAMatrixSize;
+                        rowAbove, rowBelow,
+                        rowAboveFactored    : TLAVector;
+                    begin
+                        //verify rows are valid
+                            size := getMatrixSize(matrixInOut);
+
+                            if (size.rows <= rowBelowIn) then
+                                exit();
+
+                        //get working indeces
+                            c   := rowAboveIn;
+                            rA  := c;
+                            rB  := rowBelowIn;
+
+                        //get the relevant rows
+                            rowAbove := matrixInOut[rA];
+                            rowBelow := matrixInOut[rB];
+
+                        //calculate the factor amd multiply by row above
+                            factor := rowBelow[c] / rowAbove[c];
+
+                            rowAboveFactored := vectorScalarMultiplication(factor, rowAbove);
+
+                        matrixInOut[rB] := vectorSubtraction(rowBelow, rowAboveFactored);
+                    end;
+
+            //zero column
+                procedure zeroColumn(   const colIn     : integer;
+                                        var matrixInOut : TLAMatrix );
+                    var
+                        rA, rB  : integer;
+                        size    : TLAMatrixSize;
+                    begin
+                        rA   := colIn;
+                        size := getMatrixSize(matrixInOut);
+
+                        for rB := (rA + 1) to (size.rows - 1) do
+                            factoredRowSubtraction(rA, rB, matrixInOut);
+                    end;
+
+            function gaussElimination(var matrixInOut : TLAMatrix) : boolean; //this function run the gauss elimination algorithm with an identity matrix attached to it
+                var
+                    col     : integer;
+                    size    : TLAMatrixSize;
+                procedure
+                    _insertIdentityMatrix();
+                        var
+                            i, j : integer;
+                        begin
+                            //resize the matrix to be n x 2n
+                                size := getMatrixSize(matrixInOut);
+
+                                setMatrixSize(  size.rows, (2 * size.cols),
+                                                matrixInOut                 );
+
+                            //tag the identity matrix on the right
+                                for i := 0 to (size.rows - 1) do
+                                    for j := (size.cols) to (2 * size.cols - 1) do
+                                        if ( i = (j - size.cols) ) then
+                                            matrixInOut[i][j] := 1
+                                        else
+                                            matrixInOut[i][j] := 0;
+                        end;
+                procedure
+                    _cleanMatrix();
+                        var
+                            i, j : integer;
+                        begin
+                            for i := 0 to (size.rows - 1) do
+                                for j := 0 to (2 * size.cols - 1) do
+                                    begin
+                                        if ( isAlmostZero(matrixInOut[i][j]) ) then
+                                            matrixInOut[i][j] := 0;
+                                    end;
+                        end;
+                begin
+                    //restricted to square matrices only
+                        if ( NOT(matrixIsSquare(matrixInOut)) ) then
+                            begin
+                                result := false;
+                                exit();
+                            end;
+
+                    //if the [0, 0] entry is zero gauss elimination cannot procede
+                        if ( isAlmostZero( matrixInOut[0][0] ) ) then
+                            begin
+                                result := false;
+                                exit();
+                            end;
+
+                    _insertIdentityMatrix();
+
+                    for col := 0 to (size.cols - 2) do
+                        zeroColumn(col, matrixInOut);
+
+                    _cleanMatrix();
+                end;
+
+            function matrixGaussElimination(out successfulOut   : boolean;
+                                            const matrixIn      : TLAMatrix) : TLAMatrix; overload;
+                var
+                    size                        : TLAMatrixSize;
+                    matrixOut                   : TLAMatrix;
+                begin
+                    matrixCopy(matrixIn, matrixOut);
+
+                    successfulOut := gaussElimination(matrixOut);
+
+                    if ( NOT(successfulOut) )then
+                        exit();
+
+                    size := getMatrixSize(matrixIn);
+
+                    setMatrixSize(size, matrixOut);
+
+                    result := matrixOut;
+                end;
+
+            function matrixGaussElimination(const matrixIn : TLAMatrix) : TLAMatrix; overload;
+                var
+                    dummy : boolean;
+                begin
+                    result := matrixGaussElimination(dummy, matrixIn);
+                end;
+
+        function matrixDeterminantGauss(const matrixIn : TLAMatrix) : double;
+            var
+                gaussElimMatrix : TLAMatrix;
+            begin
+
+            end;
+
         //cofactor expansion methods (Laplace)
             //sub matrix
                 function subMatrix( const rowIn, colIn  : integer;
@@ -98,114 +238,26 @@ implementation
                         result := minorOut;
                     end;
 
-            function matrixEntryCofactor(   const rowIn, colIn  : integer;
-                                            const matrixIn      : TLAMatrix) : double;
-                var
-                    i, j                : integer;
-                    entryMinor,
-                    entryCofactorOut    : double;
-                begin
-                    i := rowIn;
-                    j := colIn;
-
-                    entryMinor := matrixEntryMinor(i, j, matrixIn);
-
-                    entryCofactorOut := power( -1, (i + j) ) * entryMinor;
-
-                    result := entryCofactorOut;
-                end;
-
-        //Gauss Elimination
-            //subtract factored row
-                procedure factoredRowSubtraction(   const rowAboveIn, rowBelowIn    : integer;
-                                                    var matrixInOut                 : TLAMatrix );
+            //cofactor entry of M[i, j]
+                function matrixEntryCofactor(   const rowIn, colIn  : integer;
+                                                const matrixIn      : TLAMatrix) : double;
                     var
-                        c, rA, rB           : integer;
-                        factor              : double;
-                        size                : TLAMatrixSize;
-                        rowAbove, rowBelow,
-                        rowAboveFactored    : TLAVector;
+                        i, j                : integer;
+                        entryMinor,
+                        entryCofactorOut    : double;
                     begin
-                        //verify rows are valid
-                            size := getMatrixSize(matrixInOut);
+                        i := rowIn;
+                        j := colIn;
 
-                            if (size.rows <= rowBelowIn) then
-                                exit();
+                        entryMinor := matrixEntryMinor(i, j, matrixIn);
 
-                        //get working indeces
-                            c   := rowAboveIn;
-                            rA  := c;
-                            rB  := rowBelowIn;
+                        entryCofactorOut := power( -1, (i + j) ) * entryMinor;
 
-                        //get the relevant rows
-                            rowAbove := matrixInOut[rA];
-                            rowBelow := matrixInOut[rB];
-
-                        //calculate the factor amd multiply by row above
-                            factor := rowBelow[c] / rowAbove[c];
-
-                            rowAboveFactored := vectorScalarMultiplication(factor, rowAbove);
-
-                        matrixInOut[rB] := vectorSubtraction(rowBelow, rowAboveFactored);
+                        result := entryCofactorOut;
                     end;
 
-            //zero column
-                procedure zeroColumn(   const colIn     : integer;
-                                        var matrixInOut : TLAMatrix );
-                    var
-                        rA, rB  : integer;
-                        size    : TLAMatrixSize;
-                    begin
-                        rA   := colIn;
-                        size := getMatrixSize(matrixInOut);
 
-                        for rB := (rA + 1) to (size.rows - 1) do
-                            factoredRowSubtraction(rA, rB, matrixInOut);
-                    end;
 
-            function gaussElimination(var matrixInOut : TLAMatrix) : boolean; //this function run the gauss elimination algorithm with an identity matrix attached to it
-                var
-                    col     : integer;
-                    size    : TLAMatrixSize;
-                procedure
-                    _insertIdentityMatrix();
-                        var
-                            i, j : integer;
-                        begin
-                            //resize the matrix to be n x 2n
-                                size := getMatrixSize(matrixInOut);
-
-                                setMatrixSize(  size.rows, (2 * size.cols),
-                                                matrixInOut                 );
-
-                            //tag the identity matrix on the right
-                                for i := 0 to (size.rows - 1) do
-                                    for j := (size.cols) to (2 * size.cols - 1) do
-                                        if ( i = (j - size.cols) ) then
-                                            matrixInOut[i][j] := 1
-                                        else
-                                            matrixInOut[i][j] := 0;
-                        end;
-                begin
-                    //restricted to square matrices only
-                        if ( NOT(matrixIsSquare(matrixInOut)) ) then
-                            begin
-                                result := false;
-                                exit();
-                            end;
-
-                    //if the [0, 0] entry is zero gauss elimination cannot procede
-                        if ( isAlmostZero( matrixInOut[0][0] ) ) then
-                            begin
-                                result := false;
-                                exit();
-                            end;
-
-                    _insertIdentityMatrix();
-
-                    for col := 0 to (size.cols - 2) do
-                        zeroColumn(col, matrixInOut);
-                end;
 
 
         function determinantRec(const matrixIn : TLAMatrix) : double;
